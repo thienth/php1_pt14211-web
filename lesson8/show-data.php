adb67f1abf84d09f4b9c9e0d06738079bf2de22b
thienth32 <thienth32@gmail.com>
2019-04-08 18:01:00 +0700

    update

--- lesson8/show-data.php
+++ lesson8/show-data.php
@@ -7,9 +7,17 @@ $family_status = $_POST['family_status'];
 $favorite = $_POST['favorite'];
 $fStr = implode(', ', $favorite);
 
-var_dump($_FILES['avatar']);die;
-
+$file = $_FILES['avatar'];
 $img = "uploads/anh-cuoi.jpg";
+
+// upload file lên server
+if($file['size'] > 0){
+    $filename = uniqid() . "-" . $file['name'];
+    move_uploaded_file($file['tmp_name'], 'uploads/' . $filename);
+    $img = 'uploads/' . $filename;
+}
+
+
 ?>
 <h3><?= $student_name?></h3>
 <p>Tình trạn hôn nhân: <?= $family_status?></p>
